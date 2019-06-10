31c71ef8eded40ecb8e48f18b48cac780614778b
andressafarkas <48599122+andressafarkas@users.noreply.github.com>
2019-06-10 09:08:19 -0300

    Update administrador.php

--- administrador.php
+++ administrador.php
@@ -10,12 +10,12 @@ if(!isset($_SESSION["nome"])){
 	</head>
 	<body>
 		<form class='form1'>
-		<h4>Adicionar aluno:<h4>
-		<label for='nome'>Nome:<label>
+		<h4>Adicionar aluno:</h4>
+		<label for='nome'>Nome: </label>
 		<input type='text' name='nome' id='nome'>
-		<label for='imagem'>Imagem: <label>
+		<label for='imagem'>Imagem: </label>
 		<input type= 'file' name='imagem' id='imagem'>
-		<button class='botao'>Adicionar<button>
+		<button class='botao'>Adicionar</button>
 		</form>
 		<a href="sair.php">Sair</a>
 	</body>
