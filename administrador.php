9a176ef936cd6c54bc8127636069ef0ffb7ce7e6
soraias <48599090+soraias@users.noreply.github.com>
2019-06-11 09:03:49 -0300

    Update administrador.php

--- administrador.php
+++ administrador.php
@@ -4,19 +4,52 @@ if(!isset($_SESSION["nome"])){
 	header("location: sair.php");
 }
 ?>
-<html>
-	<head>
-		<title>Administrador</title>
-	</head>
-	<body>
-		<form class='form1'>
-		<h4>Adicionar aluno:<h4>
-		<label for='nome'>Nome:<label>
-		<input type='text' name='nome' id='nome'>
-		<label for='imagem'>Imagem: <label>
-		<input type= 'file' name='imagem' id='imagem'>
-		<button class='botao'>Adicionar<button>
-		</form>
-		<a href="sair.php">Sair</a>
-	</body>
+
+<html lang="en">
+    <head>
+        <title>Administrador</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="estilo.css" rel="stylesheet">
+    </head>
+    <body>
+		<div id="container">
+        	<form class="form">
+				<header class='header'>
+                    
+				</header>
+				<nav class='nav'>
+					<a href="sair.php">Sair</a>
+				</nav>
+				<section class='section'>
+					<h4>Adicionar aluno:<h4>
+					<label for='nome'>Nome:<label>
+					<input type='text' name='nome' id='nome'>
+					<label for='imagem'>Imagem: <label>
+					<input type='file' name='imagem' id='imagem'>
+					<button class='botao'>Adicionar<button>
+					</section>
+				<footer class='footer'>
+					<h4>Desenvolvido por Andressa, Luana e Soraia</h4>    
+				</footer>
+			</form>
+		</div> 
+    </body>
+	<?php
+		$_FILES['imagem']['name'] = $_POST['nome'];
+		mkdir($_POST['imagens']);				
+		$caminho = 'imagens/'.$_FILES['imagem']['name'];
+		move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);
+						
+	?>
+	<script>
+		var botao = document.queryselector('.botao');
+		var imagens = [];
+		
+		botao.addeventlistener('click', function(e){
+			imagens.push();	
+			
+		});
+	</script>
+						
 </html>
