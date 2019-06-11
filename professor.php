3811822625350232a24848897199886fb0cff72c
soraias <48599090+soraias@users.noreply.github.com>
2019-06-11 11:30:47 -0300

    Update professor.php

--- professor.php
+++ professor.php
@@ -4,14 +4,33 @@ if(!isset($_SESSION["nome"])){
 	header("location: sair.php");
 }
 ?>
-<html>
-	<head>
-		<title>Professor</title>
-	</head>
-	<body>
-		<form>
-		<button class='gerar'>Gerar espelho de classe<button>
-		<a href="sair.php">Sair</a>
-		</form>
-	</body>
+
+<html lang="en">
+    <head>
+        <title>Professor</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="estilo.css" rel="stylesheet">
+    </head>
+    <body>
+		<div id="container">
+        	<form class="form">
+				<header class='header'>
+                    
+                </header>
+                <nav class='nav'>
+					<a href="sair.php">Sair</a>
+                </nav>
+                <section class='section'>
+					<button class='gerar'>Gerar espelho de classe<button>
+                </section>
+                <footer class='footer'>
+                    <h4>Desenvolvido por Andressa, Luana e Soraia</h4>    
+                </footer>
+
+
+
+			</form>
+		</div>     
+    </body>
 </html>
