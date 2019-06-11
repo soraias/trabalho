b8c995499d08153712ec28e9cb343427501bd7b9
soraias <48599090+soraias@users.noreply.github.com>
2019-06-11 11:31:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
+        <title>Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="estilo.css" rel="stylesheet">
     </head>
     <body>
-        <form method="post" action="verifica.php">
-            Login: <input type="text" name="login"><br>
-            Senha: <input type="password" name="senha"><br>
-            <input type="submit" name="botao" value="Entrar">
-            </form>
+        <div id="container">
+        	<form class="form" method="post" action="verifica.php">
+                <header  class='header'>
+                    
+                </header>
+                <nav class='nav'>
+                    
+                </nav>
+                <section class='section'>
+                    Login: <input type="text" name="login"><br>
+                    Senha: <input type="password" name="senha"><br>
+                    <input type="submit" name="botao" value="Entrar">    
+                    </section>
+                <footer class='footer'>
+                    <h4>Desenvolvido por Andressa, Luana e Soraia</h4>    
+                </footer>
+			</form>
+		</div>     
     </body>
 </html>
