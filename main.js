5be412a5b4bf01a1e3962c640a095594da52f386
soraias <48599090+soraias@users.noreply.github.com>
2019-06-11 11:23:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-let botao = document.queryselector('.botao');
-let imagens = [];
-<?php mkdir($_POST['imagens']);?>
+		let botao = document.queryselector('.botao');
+		let imagens = [];
+		<?php mkdir($_POST['imagens']);?>
 	
-botao.addeventlistener('click', function(e){
-			<?php
-				$_FILES['imagem']['name'] = .$_POST['nome'];				
-				$caminho = 'imagens/'.$_FILES['imagem']['name'];
-				move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);		
-			?>
-			imagens.push(<?php $_FILES['imagem']?>);	
-});
+		botao.addeventlistener('click', function(e){
+			foreach(imagens as imagem){
+				if(<?php $_FILES['imagem']['name']?> == imagem){
+					alert("Este aluno já foi adicionado!");
+				}else{
+					<?php
+						$_FILES['imagem']['name'] = .$_POST['nome'];				
+						$caminho = 'imagens/'.$_FILES['imagem']['name'];
+						move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);		
+					?>
+					imagens.push(<?php $_FILES['imagem']?>);	
+				}
+			}
+		});
+		
