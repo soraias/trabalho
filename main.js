f4a4fe1e988cc8c3dd14baeffe84404beeeb62ad
soraias <48599090+soraias@users.noreply.github.com>
2019-06-13 10:15:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 <?php
 if($_FILES['imagem']['name'] == imagem){
-	echo "<script type='javascript'>alert('Este aluno já foi adicionado!');";
+	echo "<script type='text/javascript'>alert('Este aluno já foi adicionado!');";
 	echo "javascript:window.location='administrador.php';</script>";
 }else{
 	$_FILES['imagem']['name'] = .$_POST['nome'];				
 	$caminho = 'imagens/'.$_FILES['imagem']['name'];
 	move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);
-	echo "<script type='javascript'>alert('Aluno adicionado com sucesso!!');";
+	echo "<script type='text/javascript'>alert('Aluno adicionado com sucesso!!');";
 	echo "javascript:window.location='administrador.php';</script>";
 ?>
 
