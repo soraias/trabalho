8207acb2d796930863f7d666a110fc12e8b910bd
soraias <48599090+soraias@users.noreply.github.com>
2019-06-17 20:04:32 -0300

    Update processa.php

--- processa.php
+++ processa.php
@@ -1,61 +1,20 @@
-<script type='text/javascript'>
-
-var botao = document.queryselector('.botao');
-
-botao.addeventlistener('click', function(e){
-	e.preventdefault();
 <?php
-if($_FILES['imagem']['name'] == imagem){
-	echo "<script type='text/javascript'>alert('Este aluno já foi adicionado!');";
-	echo "javascript:window.location='administrador.php';</script>";
-}else{
-	$_FILES['imagem']['name'] = .$_POST['nome'];				
-	$caminho = 'imagens/'.$_FILES['imagem']['name'];
-	move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);
-	echo "<script type='text/javascript'>alert('Aluno adicionado com sucesso!!');";
-	echo "javascript:window.location='administrador.php';</script>";
-}
-?>;
-});
-</script>
-<?php
-$direcao = 'imagens/';
-$diretorio = opendir($direcao);
-$imagens = array();
+$partes = explode('.',$_FILES['imagem']['name']);
+$ext = end($partes);
+$imagem = $_POST['imagens']['name'].'.'.$ext;
 
 while( false !== ($filename = readdir($diretorio))){
-	array_push($imagens, $filename);
-}
-
-function gerar(){
-	shuffle($imagens);
-	foreach($imagens as $imagem){
-		echo $imagem;
-	}
-}
-if(isset($_POST) && $_POST["botao"] == "Gerar"){
-	$espelho = $_POST['espelho'];
-	if($espelho == 1){
-		session_start();
-		$_SESSION["nome"] = 'nome';
-		header("location: sala.php");
-	}else if($espelho == 2){
-		session_start();
-		$_SESSION["nome"] = 'nome';
-		header("location: laboratorio.php");
-	}
-}
-/*<script type='text/javascript'>
-	var gerar = document.queryselector('.gerar');
-	var opcao = documento.queryselector('input[name=espelho]:checked').value;
-	gerar.addeventlistener('click', funtion(e) {
-		e.preventdefault();
-		if(opcao == 1){
-			var print = <?php gerar(); ?>;
-			print.print();
-		}else if(opcao == 2){
-			var print = <?php gerar(); ?>;
-			print.print();
+	if($filename != '.'&& $filename != '..'){
+		if($_FILES['imagem']['name'] == $filename){
+			echo "<script type='text/javascript'>alert('Este aluno já foi adicionado!');";
+			echo "javascript:window.location='administrador.php';</script>";
+		}else{
+			$_FILES['imagem']['name'] = $_POST['nome'].'.'.$ext;				
+			$caminho = 'imagens/'.$_FILES['imagem']['name'];
+			move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho);
+			echo "<script type='text/javascript'>alert('Aluno adicionado com sucesso!!');";
+			echo "javascript:window.location='administrador.php';</script>";
 		}
-		
-</script>*/
+	}
+}
+?>
