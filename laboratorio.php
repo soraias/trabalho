3d321fe279f5e4184060411e8845c5739823abd9
soraias <48599090+soraias@users.noreply.github.com>
2019-06-17 20:03:40 -0300

    Update laboratorio.php

--- laboratorio.php
+++ laboratorio.php
@@ -3,4 +3,23 @@ session_start();
 if(!isset($_SESSION["nome"])){
 	header("location: sair.php");
 }
+
+$direcao = 'imagens/';
+$diretorio = opendir($direcao);
+$imagens = array();
+
+while( false !== ($filename = readdir($diretorio))){
+	if($filename != '.'&& $filename != '..'){
+		array_push($imagens, $filename);
+	}
+}
+if(isset($imagens) && count($imagens) == 0){
+	echo "<script type='text/javascript'>alert('Nenhum aluno foi adicionado!');";
+	echo "javascript:window.location='professor.php';</script>";
+}else{
+	shuffle($imagens);
+		foreach($imagens as $imagem){
+			echo '<img src='.$direcao.$imagem.'><br>';
+		}
+}
 ?>
