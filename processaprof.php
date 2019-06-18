26d4d57c2aacfcaea15e96502019350e71141bf7
soraias <48599090+soraias@users.noreply.github.com>
2019-06-17 22:35:06 -0300

    Update processaprof.php

--- processaprof.php
+++ processaprof.php
@@ -1,7 +1,10 @@
 <?php
 if(isset($_POST) && $_POST["botao"] == "Gerar"){
 	$espelho = $_POST['espelho'];
-	if($espelho == 1){
+	    if($espelho == ''){
+       		echo "<script type='text/javascript'>alert('Por favor, selecione o tipo de sala desejado!');";
+	    	echo "javascript:window.location='professor.php';</script>";
+    }else if($espelho == 1){
 		session_start();
 		$_SESSION["nome"] = 'nome';
 		header("location: sala.php");
